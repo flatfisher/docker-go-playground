cddf5e3b9f464675295b9e408e5dde862834b207
flatfisher <shimano.entou@gmail.com>
2019-03-04 21:31:33 +0900

    Back

--- main.go
+++ main.go
@@ -9,13 +9,19 @@ import (
 
 func main() {
 	log.Print("Starting on port 8080")
-
 	http.HandleFunc("/", helloHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Hello World")
 }
