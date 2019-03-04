b125234ad82aa4ab1cf94f31b93dc9121aee588c
flatfisher <shimano.entou@gmail.com>
2019-03-04 21:52:06 +0900

    Add a test code

---
+++ service/handler/handler_test.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"net/http"
+	"reflect"
+	"regexp"
+	"testing"
+)
+
+func testHandler(w http.ResponseWriter, r *http.Request) {}
+
+func TestFindHandler(t *testing.T) {
+	handler := &RegexpHandler{}
+	p := "/"
+	handler.HandleFunc(regexp.MustCompile(p), testHandler)
+	h := handler.FindHandler(p)
+	if reflect.ValueOf(h).Pointer() != reflect.ValueOf(testHandler).Pointer() {
+		t.Errorf("No matching handler found. %s", p)
+	}
+}
