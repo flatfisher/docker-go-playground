f39eb78e9f51b73d3400e2119c27de8564a2b2f5
flatfisher <shimano.entou@gmail.com>
2019-03-05 22:17:24 +0900

    Add methods

--- handlers.go
+++ handlers.go
@@ -5,10 +5,28 @@ import (
 	"net/http"
 )
 
+// TODO: 意図したHeaderやResponseが返却されるかテストコードを書く
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "Hello World")
+
+	w.Header().Set("Content-type", "text/plain; charset=utf-8'")
+	m := ""
+	switch {
+	case r.Method == http.MethodGet:
+		m = "Get"
+	case r.Method == http.MethodPost:
+		m = "Post"
+	case r.Method == http.MethodPut:
+		m = "Put"
+	case r.Method == http.MethodDelete:
+		m = "Delete"
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "405 Method Not Allowed")
+		return
+	}
+	fmt.Fprintf(w, "You requested %s request!", m)
 }
