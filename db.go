7a5d3513d29510b62793d4d0a1abcaddddb7a336
flatfisher <shimano.entou@gmail.com>
2019-03-05 20:29:22 +0900

    Update db

--- db.go
+++ db.go
@@ -2,14 +2,42 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func connect() (*sql.DB, error) {
-	db, err := sql.Open("mysql", "user:password@tcp([localhost]:3306)/database ")
+	db, err := sql.Open("mysql", dataSourceName())
 	if err != nil {
 		return nil, err
 	}
 	return db, err
+}
+
+func dataSourceName() string {
+	user := getParamString("MYSQL_USER", "user")
+	pass := getParamString("MYSQL_PASSWORD", "password")
+	protocol := getParamString("MYSQL_PROTOCOL", "tcp")
+	host := getParamString("MYSQL_DB_HOST", "localhost")
+	port := getParamString("MYSQL_PORT", "3306")
+	db := getParamString("MYSQL_DB", "database")
+	args := getParamString("MYSQL_DBARGS", " ")
+
+	if strings.Trim(args, " ") != "" {
+		args = "?" + args
+	} else {
+		args = ""
+	}
+	return fmt.Sprintf("%s:%s@%s([%s]:%s)/%s%s", user, pass, protocol, host, port, db, args)
+}
+
+func getParamString(param string, defaultValue string) string {
+	env := os.Getenv(param)
+	if env != "" {
+		return env
+	}
+	return defaultValue
 }
