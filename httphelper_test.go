30e13201266566c3e76489606b402b1b0c478561
flatfisher <shimano.entou@gmail.com>
2019-03-07 17:30:24 +0900

    Update testcode

--- httphelper_test.go
+++ httphelper_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"regexp"
 	"testing"
@@ -16,5 +18,33 @@ func TestFindHandler(t *testing.T) {
 	h := handler.FindHandler(p)
 	if reflect.ValueOf(h).Pointer() != reflect.ValueOf(testHandler).Pointer() {
 		t.Errorf("No matching handler found. %s", p)
+	}
+}
+
+type SampleServer struct{}
+
+func (s *SampleServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	m := http.NewServeMux()
+	m.HandleFunc("/", helloHandler)
+	m.ServeHTTP(w, r)
+}
+
+func TestSampleServer(t *testing.T) {
+	ts := httptest.NewServer(&SampleServer{})
+	defer ts.Close()
+	resp, err := http.Get(ts.URL + "/")
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("invalid response: %v", resp)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(body) != "You requested GET request!" {
+		t.Errorf("invalid body: %s", body)
 	}
 }
